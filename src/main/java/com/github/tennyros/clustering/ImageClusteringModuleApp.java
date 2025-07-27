277325c23b00786d36bc29e10961043524eea3af
tennyros <vvkamalov_obs@mail.ru>
2025-07-27 16:07:58 +0500

    init commit

---
+++ src/main/java/com/github/tennyros/clustering/ImageClusteringModuleApp.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.clustering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageClusteringModuleApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageClusteringModuleApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tennyros/clustering/ImageClusteringModuleAppTests.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.clustering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageClusteringModuleAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
