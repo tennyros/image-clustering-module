80a0f682dde5098ad7a242168ef1e9cf143abd74
tennyros <vvkamalov_obs@mail.ru>
2025-07-28 22:16:14 +0500

    dotenv: change DotenvLoader class for convenient properties set, according to usage environment (local or github ci)

--- src/main/java/com/github/tennyros/clustering/config/DotenvLoader.java
+++ src/main/java/com/github/tennyros/clustering/config/DotenvLoader.java
@@ -2,19 +2,27 @@ package com.github.tennyros.clustering.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.Objects;
-
+@Slf4j
 @UtilityClass
 public class DotenvLoader {
 
     public static void load() {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
-        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
-        System.setProperty("DB_BASE", Objects.requireNonNull(dotenv.get("DB_BASE")));
-        System.setProperty("DB_NAME", Objects.requireNonNull(dotenv.get("DB_NAME")));
-        System.setProperty("DB_HOST", Objects.requireNonNull(dotenv.get("DB_HOST")));
-        System.setProperty("DB_PORT", Objects.requireNonNull(dotenv.get("DB_PORT")));
+        try {
+            Dotenv dotenv = Dotenv.configure()
+                    .filename(".env")
+                    .ignoreIfMalformed()
+                    .ignoreIfMissing()
+                    .load();
+
+            dotenv.entries().forEach(entry ->
+                    System.setProperty(entry.getKey(), entry.getValue())
+            );
+
+            log.info(".env загружен и передан в System properties.");
+        } catch (Exception e) {
+            log.warn(".env не найден, полагаемся на переменные окружения.");
+        }
     }
 }
